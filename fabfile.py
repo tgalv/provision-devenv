74fd7858f6d0716316431a51a3e974f9c566522b
Tom Galvin <tom.galvin@landregistry.gov.uk>
2016-03-17 13:54:51 +0000

    First commit.

---
+++ fabfile.py
@@ -0,0 +1,82 @@
+import os
+
+from fabric.api import env, local, run, sudo, put, local, cd, lcd, task, execute, shell_env
+
+import templates
+
+
+@task
+def vagrant():
+    # change from the default user to 'vagrant'
+    env.user = 'vagrant'
+    # connect to the port-forwarded ssh
+    env.hosts = ['127.0.0.1:2222']
+    # use vagrant ssh key
+    result = local('vagrant ssh-config | grep IdentityFile', capture=True)
+    #env.key_filename = result.split()[1]
+    env.key_filename = ".vagrant//machines//default//virtualbox//private_key"
+
+
+@task
+def uname():
+    run('uname -a')
+
+
+@task
+def casework_repo(project):
+    if not os.path.exists(project):
+        local('git clone git@git.lr.net:casework/{0}.git'.format(project,))
+
+
+@task
+def build(project, branch = None):
+    with lcd(project):
+    	if branch is not None:
+    	    local("git fetch")
+            local("git checkout {0}".format(branch,))
+        local('c:\\Python34\\python.exe setup.py sdist')
+
+
+@task
+def deploy(project):
+    dirname = "./{0}/dist".format(project,)
+    fname = sorted(os.listdir(dirname))[-1]
+    put(os.path.join(dirname, fname),
+      	"/home/vagrant")
+    sudo("pip install {0}".format(fname,))
+
+
+@task
+def backup_supervisord_config():
+    sudo("mv /etc/supervisord.d /etc/supervisord.d.bak")
+    sudo("mkdir /etc/supervisord.d")
+
+
+@task
+def put_supervisor_ini(fname):
+    put(fname, "/etc/supervisord.d/", use_sudo=True)
+
+
+@task
+def supervisord_config(project, command, environment):
+    fname = "{0}.ini".format(project,)
+    ini_file = templates.render_supervisor(fname, command, environment)
+    execute(put_supervisor_ini, ini_file)
+
+
+@task
+def supervisorctl_reload():
+    sudo("supervisorctl reload")
+    sudo("supervisorctl status")
+
+
+@task
+def provision(project, branch):
+    execute(casework_repo, project)
+    execute(build, project, branch)
+    execute(deploy, project)
+    cmd = project.replace("-", "_")
+    execute(supervisord_config, project, cmd, 'process_digital_mortgage.config.DevelopmentConfig')
+    execute(supervisorctl_reload)
+
+
---
+++ templates.py
@@ -0,0 +1,20 @@
+import os
+from tempfile import NamedTemporaryFile
+ 
+
+from jinja2 import Template 
+  
+   
+SUPERVISOR_TEMPLATE = "supervisor.ini.j2"
+    
+
+def render_supervisor(fname, command, settings):
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    fqname = os.path.join(dirname, SUPERVISOR_TEMPLATE)
+    template = open(fqname).read()
+    t = Template(template)
+    ini_file = os.path.join(dirname, fname)
+    f = open(ini_file, 'w')
+    text = t.render(COMMAND=command, ENVIRONMENT=settings) 
+    f.write(text) 
+    return ini_file
