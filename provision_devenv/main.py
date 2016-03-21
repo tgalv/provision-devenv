6420a57c7a5431253246271a20e9a8ee47d6c7be
Tom Galvin <tom.galvin@landregistry.gov.uk>
2016-03-21 13:34:03 +0000

    More CL

--- provision_devenv/main.py
+++ provision_devenv/main.py
@@ -6,7 +6,7 @@ from fabric.tasks import execute
 from provision_devenv.fabfile import provision
 
 def main():
-    parser = OptionParser(usage="Usage: $path/to/devenv-root %prog name-of-project [options]")
+    parser = OptionParser(usage="Usage: $path/to/devenv-root ./provision_devenv.exe name-of-project [options]")
     parser.add_option("-b", "--branch", dest="branch", default="develop", help="Git Branch")
     parser.add_option("-p", "--port", dest="port", default="5000", help="Port")
     parser.add_option("-c", "--config", dest="config", default="Config", help="Flask Config")
