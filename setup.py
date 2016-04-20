8d59c90e3e3e5558dc654e5a3edcc1657411822f
Tom Galvin <tomgalvin@MacBook-Pro.local>
2016-04-20 14:40:16 +0100

    posix friendly setup

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-
+import os
 from setuptools import setup
 
+
+SETUP_REQUIRES = []
+if not os.name == 'posix':
+    SETUP_REQUIRES = ["py2exe"]
 
 setup(name="provision_devenv",
       version = '0.1',
       packages= ['provision_devenv'],
       install_requires = ["fabric", "jinja2"],
-      setup_requires = ["py2exe"],
+      setup_requires = SETUP_REQUIRES,
       console = ['provision_devenv/main.py'])
